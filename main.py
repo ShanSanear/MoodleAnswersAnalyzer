f1196a7cdf2182f89c1a0f4ee36ec6b9d69e6ce2
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-06-13 19:27:19 +0200

    Added typing.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import hashlib
 from pathlib import Path
+from typing import Dict, Set, Generator
 
 import htmlmin
 from bs4 import BeautifulSoup
@@ -9,22 +10,22 @@ RIGHT_ANSWER_TAG_CLASS = 'rightanswer'
 USED_PARSER = 'html.parser'
 
 
-def get_files_content(file_list):
+def get_files_content(file_list: Generator[Path]) -> Dict[str, str]:
     return {str(file): file.read_text(encoding='utf-8') for file in file_list}
 
 
-def load_soup(file_content):
+def load_soup(file_content: str) -> BeautifulSoup:
     minified_html = htmlmin.minify(file_content, remove_empty_space=True)
     return BeautifulSoup(minified_html, features=USED_PARSER)
 
 
-def normalize_answer_language(answer_tag):
+def normalize_answer_language(answer_tag: BeautifulSoup) -> BeautifulSoup:
     return BeautifulSoup(str(answer_tag).replace('The correct answer is:', 'Odpowiedź:')
                          .replace('Poprawna odpowiedź to:', 'Odpowiedź:'), features='html.parser'
                          )
 
 
-def load_tags_map(soup):
+def load_tags_map(soup: BeautifulSoup) -> Dict[bytes, Dict[str, BeautifulSoup]]:
     question_tags = soup.findAll('div', {'class': QUESTION_TAG_CLASS})
     right_answer_tags = soup.findAll('div', {'class': RIGHT_ANSWER_TAG_CLASS})
     return {
@@ -35,19 +36,18 @@ def load_tags_map(soup):
     }
 
 
-def show_already_hashed(file_path, already_hashed, currently_hashed):
+def show_already_hashed(file_path: str, already_hashed: Set[bytes], currently_hashed: Set[bytes]):
     number_of_already_hashed = len(already_hashed.intersection(currently_hashed))
     print(f"Number of already hashed questions from file: {file_path} is: {number_of_already_hashed}")
 
 
-def save_answers(whole_map):
+def save_answers(whole_map: Dict[bytes, Dict[str, BeautifulSoup]]):
     output_soup = BeautifulSoup('', features=USED_PARSER)
     for key, tags in whole_map.items():
         output_soup.append(tags['question_tag'])
         output_soup.append(tags['answer_tag'])
     output_soup = BeautifulSoup(output_soup.prettify(encoding='utf-8'), features='html.parser')
-    with Path('answers.html').open('w', encoding='utf-8') as f:
-        f.write(str(output_soup))
+    Path('answers.html').write_text(str(output_soup), encoding='utf-8')
 
 
 def main():
